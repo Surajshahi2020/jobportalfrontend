a94e8bbcdf285949860de55ee94a4808168c3abb
Hemanta-dev <www.hemantaadhikari@gmail.com>
2023-05-06 05:10:33 +0545

    Frontend Day 1 navbar banner content and lastest job card

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,43 +1,115 @@
 import React, { useState, useEffect } from "react"; import "./content.css";
+import image from '../images/image.jpg';
+
+
 
 function Content() {
-  const [jobList, setJobList] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:8000/api/v1/recruiter/job-list/")
-      .then((response) => response.json())
-      .then((data) => {
-        setJobList(data.data.docs);
-        console.log(data.data.docs);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+  // const [jobList, setJobList] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/api/v1/recruiter/job-list/")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setJobList(data.data.docs);
+  //       console.log(data.data.docs);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
   
   
   return (
-    <div className="content">
-      <h1>Find Your Dream Job</h1>
-      <form>
+    
+      <div className="content">
+      <div className="content-image">
+        <img src={image}/>
+        <h1>Find Your Dream Job</h1>
+        <form >
         <input type="text" placeholder="Job title or keyword" />
         <input type="text" placeholder="Location" />
         <button>Search</button>
       </form>
-      <h2>Featured Jobs</h2>
-      <div className="job-list">
-        {jobList.map((job) => (
-          <div className="job" key={job.id}>
-            <h3>{job.title}</h3>
-            <p>{job.start_date}</p>
-            <p>{job.end_date}</p>
-            <p>{job.company}</p>
-            <p>{job.location}</p>
-            <p>{job.experience}</p>
-            <p>{job.salary}</p>
-            <p>{job.creationdate}</p>
-            <button>Apply Now</button>
-          </div>
-        ))}
       </div>
-    </div>
+      
+    
+      <div className="container">
+        <div className="row">
+       
+
+<div className="col-md-4">
+<div className="job-list">
+  <h2>Lastest Jobs</h2>
+    {/* {jobList.map((job) => (
+      <div className="job" key={job.id}> */}
+      <div className="job-list-content">
+        <div className="job-list-content-detail">
+            <div className="job-list-content-img">
+               <img src={image} />
+    
+             </div>
+             <div className="job-list-content-list">
+                <h3>Softbenz Pvt.Ltd</h3>
+              
+                <div className="job-list-content-list-descrip">
+                   <i className="fa  fa-check "></i><p>Python Developer</p>
+                </div>
+                <div className="job-list-content-list-descrip">
+                   <i className="fa  fa-check "></i><p>3 years</p>
+                </div>
+                <div className="job-list-content-list-descrip">
+                   <i className="fa  fa-check "></i><p>Rs.30000</p>
+                </div>
+                <div className="job-list-content-list-descrip">
+                   <i className="fa  fa-check "></i><p>2023-06-01</p>
+                </div>
+               
+              
+              
+              </div>
+    
+            </div>
+         <div className="date-apply-now">
+         <div className="date now">
+          <div className="date-start-end">
+            <i className="fa  fa-calendar-plus-o"></i> <p className="date"><span>Start: </span>2023-01-06</p>
+
+         </div>
+         <div className="date-start-end">
+            <i className="fa  fa-calendar-plus-o"></i> <p className="date"><span>End: </span>2023-01-06</p>
+
+         </div>
+            </div> 
+          <div className="date-link">
+             <a>Buy Now</a>
+
+          </div>
+       
+          </div>  
+         
+        
+      </div>
+        
+        
+        
+        
+      </div>
+    {/* ))} */}
+  </div>
+
+</div>
+<div className="col-md-8">
+
+
+</div>
+  
+
+        </div>
+        
+
+      </div>
+    
+    // </div>
+
+
+   
   );
 }
 
--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import "./navbar.css";
 import logo from "../images/logo.jpg";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function Navbar() {
   return (
-    <nav>
-      <div className="logo">
-        <img src={logo} alt="Logo" />
+    <div className="main-header">
+       <div className="container">
+        <div className="row">
+         <div className="col-md-3">
+            <div className="logo">
+              <img src={logo} alt="Logo" />
+            </div>
+          </div>
+          <div className="col-md-9">
+            <ul>
+            <li>
+              <a href="https://sabaikojobs.com/"><i className="fa fa-home"></i>HOME</a>
+            </li>
+            <li>
+              <a href="https://sabaikojobs.com/"><i className="fa fa-gear"></i>SERVICE</a>
+            </li>
+            <li>
+              <a href="https://sabaikojobs.com/"><i className="fa fa-address-book"></i>CONTACT US</a>
+            </li>
+            <li>
+              <a href="https://sabaikojobs.com/"><i className="fa  fa-bullhorn"></i>ABOUT US</a>
+            </li>
+            <li>
+              <a href="https://sabaikojobs.com/login">LOGIN</a>
+            </li>
+            <li>
+              <a href="https://sabaikojobs.com/signup">SIGNUP</a>
+            </li>
+            </ul>
+          </div>
+       
+       
+ 
+
         </div>
-      <ul>
-        <li>
-          <a href="https://sabaikojobs.com/">HOME</a>
-        </li>
-        <li>
-          <a href="https://sabaikojobs.com/">SERVICE</a>
-        </li>
-        <li>
-          <a href="https://sabaikojobs.com/">CONTACT US</a>
-        </li>
-        <li>
-          <a href="https://sabaikojobs.com/">ABOUT US</a>
-        </li>
-        <li>
-          <a href="https://sabaikojobs.com/login">LOGIN</a>
-        </li>
-        <li>
-          <a href="https://sabaikojobs.com/signup">SIGNUP</a>
-        </li>
-      </ul>
-    </nav>
+     
+     </div>
+
+    </div>
+    
   );
 }
 export default Navbar;
